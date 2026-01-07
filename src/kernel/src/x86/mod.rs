045c0609716d9bc0b89faca63e8ea320b9b8c703
kix300 <kixwalkiki@gmail.com>
2026-01-07 18:23:21 +0100

    .

--- src/kernel/src/x86/mod.rs
+++ src/kernel/src/x86/mod.rs
@@ -1,10 +1,2 @@
 pub mod gdt;
 
-
-#[repr(C, packed)]
-pub struct DescriptorTable {
-	/// Size of the table in bytes, minus 1.
-	size: u16,
-	/// Linear address of the table (not physical address, paging applies).
-	offset: u32,
-}
